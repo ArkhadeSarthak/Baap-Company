6af323f72b946d6ffb60771f10a65dab7661c92c
Sarthak <arkhadesarthak2477@gmail.com>
2026-02-26 09:58:10 +0530

    Video Added

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import './compo.css';
 function Video() {
     return (
         <>
-        <video src='company-hd-Video-1.mp4' autoPlay muted loop className='baapVideo'></video>
+        <video src='./../../public/company-hd-Video-1.mp4' autoPlay muted loop className='baapVideo'></video>
             <h1 className='vdoText'>AI-Powered Digital Infrastructure</h1>
         </>
     )
